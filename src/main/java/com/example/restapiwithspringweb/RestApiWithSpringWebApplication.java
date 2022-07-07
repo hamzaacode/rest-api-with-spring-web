5c8c9e5535f10dce5348ed92f7241ae59ed21d95
hamzaacode <amir.hamza.code@gmail.com>
2022-07-07 12:01:05 +0600

    Initial Commit: REST API with Spring web

---
+++ src/main/java/com/example/restapiwithspringweb/RestApiWithSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiwithspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapiwithspringweb/RestApiWithSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapiwithspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
